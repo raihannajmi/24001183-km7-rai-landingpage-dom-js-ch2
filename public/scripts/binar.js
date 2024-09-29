70f2a99e5177139e5565ec3325e525e0d148a928
Najmi Raihan <najmiraihan@Najmis-MacBook-Pro.local>
2024-09-30 01:51:21 +0700

    I think it's fine, time for sleep huh?

--- public/scripts/binar.js
+++ public/scripts/binar.js
@@ -1,20 +1,17 @@
-// Function to get a random integer between min and max (inclusive)
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Binar class for managing car data
 class Binar {
-  // Method to populate car availability based on random conditions
-  static populateCars(cars) {
+  static populateCars = (cars) => {
     return cars.map((car) => {
       const isPositive = getRandomInt(0, 1) === 1;
       const timeAt = new Date();
       const mutator = getRandomInt(1000000, 100000000);
       const availableAt = new Date(
-        timeAt.getTime() + (isPositive ? mutator : -mutator)
+        timeAt.getTime() + (isPositive ? mutator : -1 * mutator)
       );
 
       return {
@@ -22,26 +19,27 @@ class Binar {
         availableAt,
       };
     });
-  }
+  };
 
-  // Method to list cars with optional filtering
   static async listCars(filterer) {
     let cars;
     let cachedCarsString = localStorage.getItem("CARS");
 
-    if (cachedCarsString) {
-      const cachedCars = JSON.parse(cachedCarsString);
-      cars = this.populateCars(cachedCars);
+    if (!!cachedCarsString) {
+      const cacheCars = JSON.parse(cachedCarsString);
+      cars = this.populateCars(cacheCars);
     } else {
       const response = await fetch(
         "https://raw.githubusercontent.com/fnurhidayat/probable-garbanzo/main/data/cars.min.json"
       );
       const body = await response.json();
       cars = this.populateCars(body);
+
       localStorage.setItem("CARS", JSON.stringify(cars));
     }
 
-    // Apply filter if filterer is a function
-    return filterer instanceof Function ? cars.filter(filterer) : cars;
+    if (filterer instanceof Function) return cars.filter(filterer);
+
+    return cars;
   }
 }
--- public/scripts/main.example.js
+++ public/scripts/main.example.js
@@ -6,14 +6,16 @@
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
-// Initialize app instance
+// Coba olah data ini hehe :)
+
+/*
+ * Contoh penggunaan DOM di dalam class
+ * */
 const app = new App();
 
-// Add event listener to load button
 app.loadButton.addEventListener("click", () => {
   app.clear();
   app.loadFilter().then(app.run());
 });
 
-// Initialize app
 app.init();
